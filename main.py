01c396e3927f0c5eb77984fa146a0ca608f82cf3
HDD <gptsemail.com@gmail.com>
2023-11-25 16:04:11 +0300

    Neural network

---
+++ main.py
@@ -0,0 +1,58 @@
+import joblib
+import pandas as pd
+from sklearn.calibration import calibration_curve
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import StandardScaler
+
+# Step 1: Prepare your data
+# loadet_data = pd.read_csv("fraud_dataset.csv", sep=';')
+loadet_data = pd.read_excel("fraud_dataset_1.xlsx")
+# Preprocess your data as needed
+
+# formatting data
+a = {'es_transportation': 1, 'es_health': 2, 'es_otherservices': 3, 'es_food': 4, 'es_hotelservices': 5,
+     'es_barsandrestaurants': 6,
+     'es_tech': 7, 'es_sportsandtoys': 8, 'es_wellnessandbeauty': 9, 'es_hyper': 10, 'es_fashion': 11, 'es_home': 12,
+     'es_contents': 13, 'es_travel': 14, 'es_leisure': 15}
+loadet_data['category'] = loadet_data['category'].replace(a)
+loadet_data['customer'] = loadet_data['customer'].str[1:]
+loadet_data['merchant'] = loadet_data['merchant'].str[1:]
+
+X = loadet_data.drop(['age', 'gender'], axis=1)
+
+# X = loadet_data.drop(columns=['customer', 'merchant', 'category', 'amount'])
+X = X.to_numpy()[:, (1, 2, 4, 6, 7)]
+
+y = loadet_data['fraud']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Step 3: Import necessary modules
+
+# Step 4: Create an instance of the neural network model
+model = MLPClassifier(hidden_layer_sizes=(100,), activation='relu', solver='adam')
+
+# Step 5: Train the model
+print(X_train)
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+model.fit(X_train_scaled, y_train)
+
+# Step 6: Evaluate the model
+X_test_scaled = scaler.transform(X_test)
+y_pred = model.predict(X_test_scaled)
+
+proba = model.predict_proba(X_test_scaled)[:, 1]
+y_means, proba_means = calibration_curve(y, proba, n_bins=10, strategy='quantile')
+# Step 7: Save the model
+scaler = joblib.dump(scaler, 'scaler.pkl')
+model = joblib.dump(model, 'model.pkl')
+
+# Step 8: Evaluate the performance using appropriate metric
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
+# print(y_means, proba_means)
+
+# Evaluate the performance using appropriate metriccs
---
+++ pr.py
@@ -0,0 +1,56 @@
+import joblib
+import pandas as pd
+
+model = joblib.load('model.pkl')
+scaler = joblib.load('scaler.pkl')
+# Load the new dataset
+new_data = pd.read_excel('fraud_dataset_1.xlsx')
+
+# Extract the features from the new dataset
+a = {'es_transportation': 1, 'es_health': 2, 'es_otherservices': 3, 'es_food': 4, 'es_hotelservices': 5,
+     'es_barsandrestaurants': 6,
+     'es_tech': 7, 'es_sportsandtoys': 8, 'es_wellnessandbeauty': 9, 'es_hyper': 10, 'es_fashion': 11, 'es_home': 12,
+     'es_contents': 13, 'es_travel': 14, 'es_leisure': 15}
+new_data['category'] = new_data['category'].replace(a)
+new_data['customer'] = new_data['customer'].str[1:]
+new_data['merchant'] = new_data['merchant'].str[1:]
+
+new_X = new_data.drop(['age', 'gender'], axis=1)
+new_X = new_X.to_numpy()[:, (1, 2, 4, 6, 7)]
+
+# Scale the features
+new_X_scaled = scaler.transform(new_X)
+
+# Make predictions on the new dataset
+new_y_pred = model.predict(new_X_scaled)
+new_y_pred_proba = model.predict_proba(new_X_scaled)[:, 1]
+
+print(new_y_pred)
+# Print the results
+status = []
+for row, prob in zip(new_data.iterrows(), new_y_pred_proba):
+    if new_y_pred[row[0]] == 1:
+        if 0.5 <= prob <= 0.7:
+            status.append('FREEZE')
+        elif prob > 0.7:
+            status.append('STOP')
+    elif new_y_pred[row[0]] == 0:
+        status.append('SAFE')
+
+new_data.index = range(len(new_data))
+new_data['status'] = status
+
+rev_a = {bufer: key for key, bufer in a.items()}
+new_data['category'] = new_data['category'].replace(rev_a)
+print(new_data['status'])
+
+new_data.to_excel('output.xlsx')
+
+df = pd.read_excel('output.xlsx')
+
+# Convert the DataFrame to JSON
+json_data = df.to_json(orient='records')
+
+# Save the JSON data to a file
+with open('output.json', 'w') as f:
+    f.write(json_data)
